a09f6060a65359fea076ce54da05730af255617d
Filip Kozłowski <f.kozlowski98@gmail.com>
2022-01-24 21:56:29 +0100

    vue 2

---
+++ vue/task-tracker/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2021</p>
+    <router-link v-show="homePage" to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default { name: "Footer", props: { homePage: Boolean } };
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
